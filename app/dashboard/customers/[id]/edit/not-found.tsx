1df4961c42c297a416141f552f4c8d0c09b877c0
Isaac Flores <isaacflores2205@gmail.com>
2025-11-06 12:10:16 -0600

    feat: implementar ediciÃ³n de clientes

---
+++ app/dashboard/customers/[id]/edit/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { FaceFrownIcon } from '@heroicons/react/24/outline';
+
+export default function NotFound() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center gap-2">
+      <FaceFrownIcon className="w-10 text-gray-400" />
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>No se pudo encontrar el cliente solicitado.</p>
+      <Link 
+        href="/dashboard/customers"
+        className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600"
+      >
+        Volver a Clientes
+      </Link>
+    </main>
+  );
+}
+
+
+
+
