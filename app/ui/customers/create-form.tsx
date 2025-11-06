deb9f3455e0b34d8504dba4bb23f9bb4cf4fcc45
Isaac Flores <isaacflores2205@gmail.com>
2025-11-06 12:10:11 -0600

    feat: implementar creaciÃ³n de clientes

---
+++ app/ui/customers/create-form.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import Link from 'next/link';
+import {
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
+import { createCustomer, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+
+export default function Form() {
+  const initialState: State = { message: '', errors: {} };
+  const [state, formAction] = useActionState(createCustomer, initialState);
+
+  return (
+    <form action={formAction}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Customer Name */}
+        <div className="mb-4">
+          <label htmlFor="nombre" className="mb-2 block text-sm font-medium">
+            Nombre del Cliente
+          </label>
+          <div className="relative">
+            <input
+              id="nombre"
+              name="nombre"
+              type="text"
+              placeholder="Ingrese el nombre del cliente"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              aria-describedby="nombre-error"
+              required
+            />
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="nombre-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.nombre &&
+              state.errors.nombre.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+        {/* Customer Email */}
+        <div className="mb-4">
+          <label htmlFor="correo" className="mb-2 block text-sm font-medium">
+            Correo Electrónico
+          </label>
+          <div className="relative">
+            <input
+              id="correo"
+              name="correo"
+              type="email"
+              placeholder="cliente@ejemplo.com"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              aria-describedby="correo-error"
+              required
+            />
+            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="correo-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.correo &&
+              state.errors.correo.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/customers"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create Customer</Button>
+      </div>
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {state.message && (
+          <p className="text-sm text-red-500">{state.message}</p>
+        )}
+      </div>
+    </form>
+  );
+}
+
